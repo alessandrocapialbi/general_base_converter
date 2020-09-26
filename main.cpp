661fc8444da69ac686f3b414df9e2f381a4ac1c7
Alessandro Capialbi <capialbi.alessandro@gmail.com>
2020-09-26 19:33:17 +0200

    Adding comments.

--- main.cpp
+++ main.cpp
@@ -5,26 +5,30 @@ using namespace std;
 
 int main() {
 
-    int n, c[64], i = 0, a , b;
+    int c[64], i = 0, a , b;
     char base_digits[16] =
             {'0', '1', '2', '3', '4', '5', '6', '7',
-             '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-    cout<<"Insert the number you want to convert\n\n";
+             '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'}; //All the possible digits you can have.
+
+    cout<<"Insert the number you want to convert\n\n"; //The user will digit a 10 number base.
     cin>>a;
-    cout<<"\nInsert the base you want to convert to\n\n";
+    cout<<"\nInsert the base you want to convert to\n\n"; /*The user chooses whatever base he wants to convert
+ * the number to.*/
     cin>>b;
-    do
+    do //Method of subsequent divisions.
     {
-        c[i] = a % b;
+        c[i] = a % b; //The rest will be the new converted digit into the new base.
         a/= b;
         ++i;
     }
     while(a!=0);
 
-    --i;
+    --i; /*Since an array starts from index 0, we must substract 1 to the total dimension;
+ * e.g. array[6] has indexes 0, 1, 2, 3, 4, 5.*/
     for(  ; i>=0; i--)
     {
-        cout << base_digits[c[i]];
+        cout << base_digits[c[i]]; /*I go backward through the array in order to display
+ * the digits in the correct way. I use base_digits, so I can access to every possible digit of every base. */
     }
     cout<<endl;
     system("pause");
